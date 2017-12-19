2a54f4fcb4437509ccc9ae92658654155730d829
ddbrennan <ddbren@gmail.com>
2017-12-19 14:45:28 -0500

    song form complete

--- spec/features/song_form_spec.rb
+++ spec/features/song_form_spec.rb
@@ -30,8 +30,8 @@ describe "the song form", :type => :feature do
   it 'creates a song with notes' do
     visit '/songs/new'
     fill_in :song_title, with: 'Little Earthquakes'
-    fill_in :song_notes_1, with: 'great piano'
-    fill_in :song_notes_2, with: 'inaccurate seismology'
+    fill_in :song_note_contents_1, with: 'great piano'
+    fill_in :song_note_contents_2, with: 'inaccurate seismology'
     find('input[name="commit"]').click
     expect(Song.last.note_contents).to eq ['great piano', 'inaccurate seismology']
   end
